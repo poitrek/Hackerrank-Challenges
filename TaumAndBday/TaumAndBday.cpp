986ba5da00a6acdad2b25519a1c16e31de388669
poitrek <22658450+poitrek@users.noreply.github.com>
2021-05-30 15:57:34 +0200

    TaumAndBday problem

---
+++ TaumAndBday/TaumAndBday.cpp
@@ -0,0 +1,24 @@
+/*
+Challenge: Taum And B'day
+Level: Easy
+https://www.hackerrank.com/challenges/taum-and-bday/problem
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int taumBday(int b, int w, int bc, int wc, int z) {
+	
+}
+
+int main() {
+	int t;
+	cin >> t;
+	vector<int> b(t, 0), w(t, 0), bc(t, 0), wc(t, 0), z(t, 0);
+	for (int i = 0; i < t; i++) {
+		cin >> b[i] >> w[i] >> bc[i] >> wc[i] >> z[i];
+	}
+	return 0;
+}
